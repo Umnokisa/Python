7fb5c6fa624eea2693a3cb85176f585ce1e2339e
Umnokisa <kudahtalka@gmail.com>
2023-04-25 18:34:22 +0300

    Sem8_HW

--- Sem_8/Task_38.py
+++ Sem_8/Task_38.py
@@ -1,69 +1,35 @@
-# path = 'файл.txt'
-
-# try:                        
-#     file = open(path, 'r')  
-# except IOError:             
-#     print('Создан новый справочник -> файл.txt ')
-#     file = open(path, 'w')
-# finally:                    
-#     file.close()
-
-# actions = {'1': 'чтение',
-#            '2': 'запись',
-#            '3': 'поиск',
-#            '4': 'изменение',
-#            '5': 'удаление',
-#            'q': 'выход'
-#            }
-
-# action = None
-# while action != 'q':
-#     print('Какое действие вы хотите совершить?', *[f'{i} - {actions[i]}' for i in actions])
-#     action = input()
-#     if action == '1':
-#         read_cont()
-#     elif action == '2':
-#         add_cont()
-#     elif action == '3':
-#         find_cont()
-#     elif action == '4':
-#         change_cont()
-#     elif action == '5':
-#         del_cont()
-
-
 # добавляет новую строку
-# def add_cont():
-#     file = open ('файл.txt', 'a', encoding='UTF-8')
-#     data1 = input('Введите ФИО: ')
-#     data2 = input('Введите номер телефона: ')
-#     data3 = input('Введите комментарий: ')
-#     data = '\n' + data1 + '; ' + data2 + '; ' + data3 
-#     print(data)
-#     file.write(data)
-#     file.close()
+def add_cont():
+    file = open ('файл.txt', 'a', encoding='UTF-8')
+    data1 = input('Введите ФИО: ')
+    data2 = input('Введите номер телефона: ')
+    data3 = input('Введите комментарий: ')
+    data = '\n' + data1 + '; ' + data2 + '; ' + data3 
+    print(data)
+    file.write(data)
+    file.close()
 
 # показывает справочник целиком
-# def read_cont():
-#     file = open('файл.txt', 'r', encoding='UTF-8')
-#     data = file.readlines()
-#     file.close()
-#     for contact in data:
-#         print(contact)
-# read_cont()
-# file.close()
+def read_cont():
+    file = open('файл.txt', 'r', encoding='UTF-8')
+    data = file.readlines()
+    file.close()
+    for contact in data:
+        print(contact)
+read_cont()
+
 
 # поиск информации в справочнике
-# def find_cont():
-#     file = open('файл.txt', 'r', encoding='UTF-8')
-#     data = file.readlines()
-#     file.close()
-#     find_cont = input('Введите информацию для поиска: ')
-#     for cont in data:
-#         if find_cont.lower() in cont.lower():
-#             print(cont)
-# find_cont()        
-# file.close()
+def find_cont():
+    file = open('файл.txt', 'r', encoding='UTF-8')
+    data = file.readlines()
+    file.close()
+    find_cont = input('Введите информацию для поиска: ')
+    for cont in data:
+        if find_cont.lower() in cont.lower():
+            print(cont)
+find_cont()        
+
 
 # изменяет данные
 def change_cont():
@@ -103,15 +69,48 @@ def change_cont():
 
 
 # Удаляет данные из телефонной книги    
-# def del_cont():
-#     with open('файл.txt', 'r', encoding="utf-8") as f:
-#         X = input('Введите Имя или Фамилию для удаления: ')
-#         lines = f.readlines()
-#         with open('файл.txt', 'w', encoding="utf-8") as f:
-#             for line in lines:
-#                 if X in line:
-#                     print("Строка удалена")
-#                 else:
-#                     print(line)    
-#                     f.write(line)
-#             file.close()
+def del_cont():
+    with open('файл.txt', 'r', encoding="utf-8") as f:
+        X = input('Введите Имя или Фамилию для удаления: ')
+        lines = f.readlines()
+        with open('файл.txt', 'w', encoding="utf-8") as f:
+            for line in lines:
+                if X in line:
+                    print("Строка удалена")
+                else:
+                    print(line)    
+                    f.write(line)
+            file.close()
+
+path = 'файл.txt'
+
+try:                        
+    file = open(path, 'r')  
+except IOError:             
+    print('Создан новый справочник -> файл.txt ')
+    file = open(path, 'w')
+finally:                    
+    file.close()
+
+actions = {'1': 'чтение',
+           '2': 'запись',
+           '3': 'поиск',
+           '4': 'изменение',
+           '5': 'удаление',
+           'q': 'выход'
+           }
+
+action = None
+while action != 'q':
+    print('Какое действие вы хотите совершить?', *[f'{i} - {actions[i]}' for i in actions])
+    action = input(path)
+    if action == '1':
+        read_cont(path)
+    elif action == '2':
+        add_cont(path)
+    elif action == '3':
+        find_cont(path)
+    elif action == '4':
+        change_cont(path)
+    elif action == '5':
+        del_cont(path)
